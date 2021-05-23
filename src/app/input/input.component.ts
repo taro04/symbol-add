6c0ee6ab2a0390c80b7f785af4f9c969eb74b0cb
taro04 <75844498+taro04@users.noreply.github.com>
2021-05-23 19:14:16 +0900

    sss

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestApiService } from "../rest-api.service"
 import { MessagesService } from "../messages.service"
 import { AccountInfo } from "symbol-sdk"
+import * as SYMSDK from "symbol-sdk"
 
 @Component({
   selector: 'app-input',
@@ -14,25 +15,32 @@ export class InputComponent implements OnInit {
   numx:number = 0
   gettxt:string = ""
   symbolAddress? :AccountInfo  
-
+  //imp? :SYMSDK.UInt64
+  imp? :SYMSDK.UInt64
+  mscs? :SYMSDK.Mosaic[]
+  publicKey? :string
 
   constructor(
-    private restApiService:RestApiService
-//    public messageService:MessagesService
+    private restApiService:RestApiService,
+    public messageService:MessagesService
   ) { }
 
   ngOnInit(): void {
   }
 
+  //sdkで取得。
   check(address :string): void{
     this.addressX=address
     this.numx++
-    //this.restApiService.
-    this.restApiService.log(`check exe ${this.numx} times ${this.addressX}`)
+    this.messageService.add(`check exe ${this.numx} times ${this.addressX}`)
     this.restApiService.get_my_AccountInfo2(address)
     .subscribe(getAdr => this.symbolAddress = getAdr)
+    this.imp = this.symbolAddress?.importance
+    this.mscs = this.symbolAddress?.mosaics
+    this.publicKey = this.symbolAddress?.publicKey
   }
   
+  //apiでaccountsを取得。
   getAccounts():void{
     this.restApiService.getAccounts()
         .subscribe(jsonfile => this.gettxt = jsonfile); //jsonfileは.subscribeの引数
@@ -42,5 +50,5 @@ export class InputComponent implements OnInit {
   settxt():void{
     this.gettxt="set"
   }
-
+  
 }
